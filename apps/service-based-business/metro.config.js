97286c6280dc4b11f8586aa493334c9c2e08f004
Symbiot <spandayapp@gmail.com>
2025-07-31 10:47:33 +0300

    populate app assets

--- apps/service-based-business/metro.config.js
+++ apps/service-based-business/metro.config.js
@@ -5,14 +5,7 @@ const { mergeConfig } = require('metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
 const customConfig = {
-  cacheVersion: 'service-based-business',
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
@@ -23,11 +16,7 @@ const customConfig = {
 };
 
 module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
-  // Change this to true to see debugging info.
-  // Useful if you have issues resolving modules
   debug: false,
-  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
   extensions: [],
-  // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
   watchFolders: [],
 });

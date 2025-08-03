57a8c5c0988e7037f1ee1fb0f96506cc735701e2
Symbiot <spandayapp@gmail.com>
2025-08-03 08:27:12 +0300

    update accordion styles

--- libs/ui/src/lib/collapsible/accordion.tsx
+++ libs/ui/src/lib/collapsible/accordion.tsx
@@ -1,9 +1,10 @@
 import { View, XStack } from 'tamagui';
 import Collapsible from 'react-native-collapsible';
-import { useCallback, useState } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { MediumText, RegularText } from '../text/text';
 import { Icon } from '../icons';
 import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics';
+import { Card } from '../card/card';
 
 type AccordionItem = {
   title: string;
@@ -11,40 +12,63 @@ type AccordionItem = {
 };
 
 export const Accordion = ({ items }: { items: AccordionItem[] }) => {
-  return items.map((item, index) => (
-    <Item key={index} item={item} last={index === items.length - 1} />
-  ));
-};
-
-const Item = ({ item, last }: { item: AccordionItem; last: boolean }) => {
-  const [collapsed, setCollapsed] = useState(true);
-
-  const toggle = useCallback(() => {
-    setCollapsed((prev) => !prev);
-    void impactAsync(ImpactFeedbackStyle.Soft);
-  }, []);
-
   return (
-    <View borderBottomWidth={last ? 0 : 1} borderBottomColor="$background1">
-      <XStack
-        cursor="pointer"
-        gap="$5"
-        justifyContent="space-between"
-        padding="$3"
-        onPress={toggle}
-      >
-        <MediumText flex={1} fontSize={20}>
-          {item.title}
-        </MediumText>
-
-        <Icon name={collapsed ? 'AltArrowDown' : 'AltArrowUp'} />
-      </XStack>
-
-      <Collapsible collapsed={collapsed}>
-        <View padding="$3">
-          <RegularText>{item.text}</RegularText>
-        </View>
-      </Collapsible>
-    </View>
+    <Card>
+      {items.map((item, index) => (
+        <Item
+          key={index}
+          item={item}
+          first={index === 0}
+          last={index === items.length - 1}
+        />
+      ))}
+    </Card>
   );
 };
+
+const Item = memo(
+  ({
+    item,
+    first,
+    last,
+  }: {
+    item: AccordionItem;
+    first: boolean;
+    last: boolean;
+  }) => {
+    const paddingTop = first ? 0 : '$3';
+    const paddingBottom = last ? 0 : '$3';
+
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggle = useCallback(() => {
+      setCollapsed((prev) => !prev);
+      void impactAsync(ImpactFeedbackStyle.Soft);
+    }, []);
+
+    return (
+      <View borderBottomWidth={last ? 0 : 1} borderBottomColor="$background">
+        <XStack
+          cursor="pointer"
+          gap="$5"
+          paddingTop={paddingTop}
+          paddingBottom={paddingBottom}
+          justifyContent="space-between"
+          onPress={toggle}
+        >
+          <MediumText flex={1} fontSize={20}>
+            {item.title}
+          </MediumText>
+
+          <Icon name={collapsed ? 'AltArrowDown' : 'AltArrowUp'} />
+        </XStack>
+
+        <Collapsible collapsed={collapsed}>
+          <RegularText paddingTop={paddingTop} paddingBottom={paddingBottom}>
+            {item.text}
+          </RegularText>
+        </Collapsible>
+      </View>
+    );
+  },
+);

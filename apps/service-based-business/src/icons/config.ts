8b0bf24b5bc793ed4336bb866fb368b5a91025b4
Symbiot <spandayapp@gmail.com>
2025-08-14 21:12:48 +0300

    enhance plus tab styles

---
+++ apps/service-based-business/src/icons/config.ts
@@ -0,0 +1,10 @@
+import { IconName } from '@symbiot-core-apps/ui';
+
+export const Icons: Record<string, IconName> = {
+  Home: 'Home',
+  Notifications: 'Bell',
+  Calendar: 'Calendar',
+  TabsPlus: 'AddCircle',
+  Workspace: 'MagicStick',
+  More: 'Widget3',
+};

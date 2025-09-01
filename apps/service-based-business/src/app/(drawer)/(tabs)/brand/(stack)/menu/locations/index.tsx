afe5aa3aabd8492fe64cdf6ee33cb98e76a3e90b
Symbiot <spandayapp@gmail.com>
2025-09-01 23:16:29 +0300

    enhance current brand locations list

--- apps/service-based-business/src/app/(drawer)/(tabs)/brand/(stack)/menu/locations/index.tsx
+++ apps/service-based-business/src/app/(drawer)/(tabs)/brand/(stack)/menu/locations/index.tsx
@@ -1,26 +1,3 @@
 import { CurrentBrandLocations } from '@symbiot-core-apps/brand-location';
-import { Redirect } from 'expo-router';
-import { useCurrentBrandLocationsQuery } from '@symbiot-core-apps/api';
-import { LoadingView } from '@symbiot-core-apps/ui';
-import { useCurrentBrandState } from '@symbiot-core-apps/state';
-import { useMemo } from 'react';
 
-export default () => {
-  const { data } = useCurrentBrandLocationsQuery();
-  const { brand } = useCurrentBrandState();
-
-  const locations = useMemo(
-    () => data?.items || brand?.locations,
-    [data?.items, brand?.locations],
-  );
-
-  if (!locations) {
-    return <LoadingView />;
-  }
-
-  if (!locations.length) {
-    return <Redirect href="/brand/location/create" />;
-  }
-
-  return <CurrentBrandLocations locations={locations} />;
-};
+export default CurrentBrandLocations;
--- libs/brand-location/src/lib/components/current-brand-locations.tsx
+++ libs/brand-location/src/lib/components/current-brand-locations.tsx
@@ -1,60 +1,89 @@
 import {
+  AnimatedList,
   Avatar,
   FormView,
   Icon,
+  InitView,
   PageView,
   RegularText,
   SemiBoldText,
+  useScreenHeaderHeight,
 } from '@symbiot-core-apps/ui';
 import { useCurrentBrandState } from '@symbiot-core-apps/state';
-import { View, XStack } from 'tamagui';
+import { View } from 'tamagui';
 import { emitHaptic } from '@symbiot-core-apps/shared';
-import { BrandLocation } from '@symbiot-core-apps/api';
-import { router } from 'expo-router';
+import {
+  BrandLocation,
+  useCurrentBrandLocationsQuery,
+} from '@symbiot-core-apps/api';
+import { Redirect, router } from 'expo-router';
+import { useCallback, useMemo } from 'react';
 
-export const CurrentBrandLocations = ({
-  locations,
-}: {
-  locations: BrandLocation[];
-}) => {
+export const CurrentBrandLocations = () => {
   const { brand } = useCurrentBrandState();
+  const headerHeight = useScreenHeaderHeight();
+  const { data, isLoading, isRefetching, error, refetch } =
+    useCurrentBrandLocationsQuery();
+
+  const locations = useMemo(
+    () => data?.items || brand?.locations,
+    [data?.items, brand?.locations],
+  );
+
+  const renderItem = useCallback(
+    ({ item }: { item: BrandLocation }) => (
+      <FormView
+        alignItems="center"
+        backgroundColor="$background1"
+        borderRadius="$10"
+        padding="$4"
+        gap="$4"
+        cursor="pointer"
+        flexDirection="row"
+        pressStyle={{ opacity: 0.8 }}
+        onPress={() => {
+          emitHaptic();
+          router.push(`/brand/employee/update/${item.id}`);
+        }}
+      >
+        <Avatar name={item.name} size={40} url={item.avatarXsUrl} />
+
+        <View gap="$1" flex={1}>
+          <SemiBoldText numberOfLines={1}>{item.name}</SemiBoldText>
+          <RegularText color="$placeholderColor" numberOfLines={1}>
+            {item.address}
+          </RegularText>
+        </View>
+
+        <Icon name="ArrowRight" />
+      </FormView>
+    ),
+    [],
+  );
+
+  if (!locations) {
+    return <InitView loading={isLoading} error={error} />;
+  }
+
+  if (!locations.length) {
+    return <Redirect href="/brand/location/create" />;
+  }
 
   return (
-    <PageView scrollable withHeaderHeight gap={2}>
-      <FormView gap={2}>
-        {locations.map((location) => (
-          <XStack
-            key={location.id}
-            backgroundColor="$background1"
-            borderRadius="$10"
-            padding="$4"
-            alignItems="center"
-            gap="$4"
-            cursor="pointer"
-            pressStyle={{ opacity: 0.8 }}
-            onPress={() => {
-              emitHaptic();
-              router.push(`/brand/location/update/${location.id}`);
-            }}
-          >
-            <Avatar
-              name={location.name}
-              size={40}
-              url={location.avatarXsUrl || brand?.avatarXsUrl}
-              color={brand?.avatarColor}
-            />
-
-            <View gap="$1" flex={1}>
-              <SemiBoldText numberOfLines={1}>{location.name}</SemiBoldText>
-              <RegularText color="$placeholderColor" numberOfLines={1}>
-                {location.address}
-              </RegularText>
-            </View>
-
-            <Icon name="ArrowRight" />
-          </XStack>
-        ))}
-      </FormView>
+    <PageView ignoreTopSafeArea ignoreBottomSafeArea paddingBottom={0}>
+      <AnimatedList
+        refreshing={isRefetching && !isLoading}
+        data={locations}
+        progressViewOffset={headerHeight}
+        contentContainerStyle={{
+          gap: 2,
+          paddingTop: headerHeight,
+          paddingBottom: 100,
+        }}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        onRefresh={refetch}
+      />
     </PageView>
   );
 };

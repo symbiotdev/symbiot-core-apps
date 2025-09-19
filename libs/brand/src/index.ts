9ecfac2dd371ad50699502e146aad982ff14e546
Symbiot <spandayapp@gmail.com>
2025-09-19 15:32:29 +0300

    enhance update brand

--- libs/brand/src/index.ts
+++ libs/brand/src/index.ts
@@ -1,5 +1,6 @@
-export * from './lib/components/update-brand';
 export * from './lib/components/create-brand';
+export * from './lib/components/create-brand-intro';
 export * from './lib/components/my-brands-selection-list';
+export * from './lib/components/update-brand';
 
 export * from './lib/hooks/use-brand-auth';

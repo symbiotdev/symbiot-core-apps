922c9712e151dfdd3693c5e65573255626c93abf
Symbiot <spandayapp@gmail.com>
2025-12-04 20:45:49 +0200

    handle more notifications

--- apps/service-based-business/src/utils/notification.ts
+++ apps/service-based-business/src/utils/notification.ts
@@ -1,48 +1,143 @@
 import {
   BrandBookingType,
+  BrandMembershipType,
   Notification,
   NotificationType,
 } from '@symbiot-core-apps/api';
 import { router } from 'expo-router';
+
+const goHome = () =>
+  router.canGoBack() ? router.back() : router.navigate('/');
 
 export const onPressNotification = (notification: Notification) => {
   if (!notification?.type) {
     return;
   }
 
-  if (notification.type === NotificationType.welcome) {
-    if (router.canGoBack()) {
-      router.back();
-    } else {
-      router.navigate('/');
-    }
+  if (
+    [
+      NotificationType.welcome,
+      NotificationType.accountSubscriptionCreated,
+      NotificationType.accountSubscriptionRemoved,
+      NotificationType.brandEmployeeAssigned,
+      NotificationType.brandEmployeeUnassigned,
+    ].includes(notification.type)
+  ) {
+    goHome();
+  } else if (
+    [NotificationType.brandCreated, NotificationType.brandUpdated].includes(
+      notification.type,
+    )
+  ) {
+    router.navigate('/brand/profile');
   } else if (
     [
-      NotificationType.unavailableBrandBookingsCreated,
-      NotificationType.unavailableBrandBookingsCanceled,
-      NotificationType.unavailableBrandBookingsUpdated,
-      NotificationType.unavailableBrandBookingsUpcoming,
+      NotificationType.brandLocationCreated,
+      NotificationType.brandLocationUpdated,
+      NotificationType.brandLocationRemoved,
     ].includes(notification.type)
   ) {
     router.navigate(
-      `/bookings/${BrandBookingType.unavailable}/${notification.brandBookingId}/profile`,
+      notification.brandLocation
+        ? `/locations/${notification.brandLocation.id}/profile`
+        : '/locations',
     );
   } else if (
     [
-      NotificationType.serviceBrandBookingsCanceled,
-      NotificationType.serviceBrandBookingsCreated,
-      NotificationType.serviceBrandBookingsUpdated,
-      NotificationType.serviceBrandBookingsUpcoming,
+      NotificationType.brandEmployeeCreated,
+      NotificationType.brandEmployeeUpdated,
+      NotificationType.brandEmployeeRemoved,
+    ].includes(notification.type)
+  ) {
+    router.navigate(
+      notification.brandEmployee
+        ? `/employees/${notification.brandEmployee.id}/profile`
+        : '/employees',
+    );
+  } else if (
+    [
+      NotificationType.brandPeriodMembershipCreated,
+      NotificationType.brandPeriodMembershipUpdated,
+      NotificationType.brandPeriodMembershipRemoved,
+    ].includes(notification.type)
+  ) {
+    router.navigate(
+      notification.brandMembership
+        ? `/memberships/${notification.brandMembership.id}/profile`
+        : `/memberships/${BrandMembershipType.period}`,
+    );
+  } else if (
+    [
+      NotificationType.brandVisitMembershipCreated,
+      NotificationType.brandVisitMembershipUpdated,
+      NotificationType.brandVisitMembershipRemoved,
+    ].includes(notification.type)
+  ) {
+    router.navigate(
+      notification.brandMembership
+        ? `/memberships/${notification.brandMembership.id}/profile`
+        : `/memberships/${BrandMembershipType.visits}`,
+    );
+  } else if (
+    [
+      NotificationType.brandServiceCreated,
+      NotificationType.brandServiceUpdated,
+      NotificationType.brandServiceRemoved,
+    ].includes(notification.type)
+  ) {
+    router.navigate(
+      notification.brandService
+        ? `/services/${notification.brandService.id}/profile`
+        : '/services',
+    );
+  } else if (
+    [
+      NotificationType.brandClientPeriodMembershipCreated,
+      NotificationType.brandClientPeriodMembershipUpdated,
+      NotificationType.brandClientPeriodMembershipRemoved,
+      NotificationType.brandClientVisitMembershipCreated,
+      NotificationType.brandClientVisitMembershipUpdated,
+      NotificationType.brandClientVisitMembershipRemoved,
+    ].includes(notification.type)
+  ) {
+    router.navigate(
+      notification.brandClient
+        ? `/clients/${notification.brandClient.id}/profile`
+        : '/clients',
+    );
+  } else if (
+    [
+      NotificationType.unavailableBrandBookingCreated,
+      NotificationType.unavailableBrandBookingCanceled,
+      NotificationType.unavailableBrandBookingUpdated,
+      NotificationType.unavailableBrandBookingUpcoming,
+    ].includes(notification.type)
+  ) {
+    router.navigate(
+      notification.brandBooking
+        ? `/bookings/${BrandBookingType.unavailable}/${notification.brandBooking.id}/profile`
+        : `/bookings/${BrandBookingType.unavailable}/`,
+    );
+  } else if (
+    [
+      NotificationType.serviceBrandBookingCanceled,
+      NotificationType.serviceBrandBookingCreated,
+      NotificationType.serviceBrandBookingUpdated,
+      NotificationType.serviceBrandBookingUpcoming,
       NotificationType.serviceBrandBookingClientAdded,
       NotificationType.serviceBrandBookingClientRemoved,
     ].includes(notification.type)
   ) {
     router.navigate(
-      `/bookings/${BrandBookingType.service}/${notification.brandBookingId}/profile`,
+      notification.brandBooking
+        ? `/bookings/${BrandBookingType.service}/${notification.brandBooking.id}/profile`
+        : `/bookings/${BrandBookingType.service}`,
     );
   } else {
     if (process.env.EXPO_PUBLIC_APP_MODE !== 'production') {
       alert('Handle it');
     }
+
+    goHome();
   }
 };
--- libs/api/src/lib/types/notification.ts
+++ libs/api/src/lib/types/notification.ts
@@ -1,30 +1,80 @@
 import { Account } from './account';
 import { Brand } from './brand';
+import { BrandClient, BrandClientMembership } from './brand-client';
+import { BrandBooking } from './brand-booking';
+import { BrandLocation } from './brand-location';
+import { BrandEmployee } from './brand-employee';
+import { BrandService } from './brand-service';
+import { BrandMembership } from './brand-membership';
 
 export enum NotificationType {
   welcome = 'welcome',
   reminder = 'reminder',
-  // brand booking
-  unavailableBrandBookingsCreated = 'unavailable_brand_bookings_created',
-  unavailableBrandBookingsCanceled = 'unavailable_brand_bookings_canceled',
-  unavailableBrandBookingsUpdated = 'unavailable_brand_bookings_updated',
-  unavailableBrandBookingsUpcoming = 'unavailable_brand_bookings_upcoming',
-  serviceBrandBookingsCanceled = 'service_brand_bookings_canceled',
-  serviceBrandBookingsCreated = 'service_brand_bookings_created',
-  serviceBrandBookingsUpdated = 'service_brand_bookings_updated',
-  serviceBrandBookingsUpcoming = 'service_brand_bookings_upcoming',
+  // account subscription
+  accountSubscriptionCreated = 'account_subscription_created',
+  accountSubscriptionRemoved = 'account_subscription_removed',
+  // brand
+  brandCreated = 'brand_created',
+  brandUpdated = 'brand_updated',
+  // brand location
+  brandLocationCreated = 'brand_location_created',
+  brandLocationUpdated = 'brand_location_updated',
+  brandLocationRemoved = 'brand_location_removed',
+  // brand employee
+  brandEmployeeAssigned = 'brand_employee_assigned',
+  brandEmployeeUnassigned = 'brand_employee_unassigned',
+  brandEmployeeCreated = 'brand_employee_created',
+  brandEmployeeUpdated = 'brand_employee_updated',
+  brandEmployeeRemoved = 'brand_employee_removed',
+  // brand service
+  brandServiceCreated = 'brand_service_created',
+  brandServiceUpdated = 'brand_service_updated',
+  brandServiceRemoved = 'brand_service_removed',
+  // brand period membership
+  brandPeriodMembershipCreated = 'brand_period_membership_created',
+  brandPeriodMembershipUpdated = 'brand_period_membership_updated',
+  brandPeriodMembershipRemoved = 'brand_period_membership_removed',
+  // brand visit membership
+  brandVisitMembershipCreated = 'brand_visit_membership_created',
+  brandVisitMembershipUpdated = 'brand_visit_membership_updated',
+  brandVisitMembershipRemoved = 'brand_visit_membership_removed',
+  // brand client period membership
+  brandClientPeriodMembershipCreated = 'brand_client_period_membership_created',
+  brandClientPeriodMembershipUpdated = 'brand_client_period_membership_updated',
+  brandClientPeriodMembershipRemoved = 'brand_client_period_membership_removed',
+  // brand client visit membership
+  brandClientVisitMembershipCreated = 'brand_client_visit_membership_created',
+  brandClientVisitMembershipUpdated = 'brand_client_visit_membership_updated',
+  brandClientVisitMembershipRemoved = 'brand_client_visit_membership_removed',
+  // brand unavailable booking
+  unavailableBrandBookingCreated = 'unavailable_brand_booking_created',
+  unavailableBrandBookingCanceled = 'unavailable_brand_booking_canceled',
+  unavailableBrandBookingUpdated = 'unavailable_brand_booking_updated',
+  unavailableBrandBookingUpcoming = 'unavailable_brand_booking_upcoming',
+  // brand service booking
+  serviceBrandBookingCanceled = 'service_brand_booking_canceled',
+  serviceBrandBookingCreated = 'service_brand_booking_created',
+  serviceBrandBookingUpdated = 'service_brand_booking_updated',
+  serviceBrandBookingUpcoming = 'service_brand_booking_upcoming',
+  // brand service booking client
   serviceBrandBookingClientAdded = 'service_brand_booking_client_added',
   serviceBrandBookingClientRemoved = 'service_brand_booking_client_removed',
 }
 
 export type Notification = {
   id: string;
-  type: NotificationType;
-  from: Account;
-  brand?: Brand;
   title: string;
   subtitle: string;
   read: boolean;
-  brandBookingId: string;
-  cAt: Date;
+  cAt: string;
+  type: NotificationType;
+  from: Account;
+  brand?: Brand;
+  brandLocation?: BrandLocation;
+  brandEmployee?: BrandEmployee;
+  brandService?: BrandService;
+  brandMembership?: BrandMembership;
+  brandClient?: BrandClient;
+  brandClientMembership?: BrandClientMembership;
+  brandBooking?: BrandBooking;
 };

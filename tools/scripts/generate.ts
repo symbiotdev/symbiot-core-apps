15c39b01ed65e36574f512a095284039f2096772
Symbiot <spandayapp@gmail.com>
2026-01-22 12:48:17 +0200

    update generate command

--- tools/scripts/generate.ts
+++ tools/scripts/generate.ts
@@ -1,6 +1,11 @@
 import { input, select } from '@inquirer/prompts';
 import { spawn } from 'child_process';
 
+enum GenerateEntity {
+  app = 'app',
+  library = 'library',
+}
+
 (async () => {
   const plugin = await select({
     message: 'Select plugin',
@@ -12,23 +17,22 @@ import { spawn } from 'child_process';
     ],
   });
 
-  const type = await select({
+  const type = await select<GenerateEntity>({
     message: 'Select type',
     choices: [
       {
         name: 'Application',
-        value: 'app',
+        value: GenerateEntity.app,
       },
       {
         name: 'Library',
-        value: 'library',
+        value: GenerateEntity.library,
       },
     ],
   });
 
   const name = await input({ message: 'Enter name' });
-
-  const directory = type === 'app' ? 'apps' : 'libs';
+  const directory = type === GenerateEntity.app ? 'apps' : 'libs';
 
   spawn(`nx g ${plugin}:${type} ${name} --directory=${directory}/${name}`, {
     stdio: 'inherit',

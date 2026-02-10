d0228a8172a54b2901ecc1f3a5303fecd161ff38
Symbiot <spandayapp@gmail.com>
2026-02-10 19:06:37 +0200

    update download media files method

--- libs/shared/src/lib/utils/file.ts
+++ libs/shared/src/lib/utils/file.ts
@@ -1,21 +1,64 @@
-export function downloadArrayBuffer(
+import { Platform } from 'react-native';
+import { cacheDirectory, writeAsStringAsync } from 'expo-file-system';
+import { Asset } from 'expo-asset';
+import { shareAsync } from 'expo-sharing';
+import { ShowNativeFailedAlert } from './burnt';
+
+export async function downloadArrayBuffer(
   arrayBuffer: ArrayBufferLike,
   fileName: string,
-  mimeType = 'application/octet-stream',
 ) {
-  const blob = new Blob([arrayBuffer], { type: mimeType });
-  const url = URL.createObjectURL(blob);
+  if (Platform.OS === 'web') {
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
 
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = fileName;
-  a.style.display = 'none';
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.style.display = 'none';
 
-  document.body.appendChild(a);
-  a.click();
+    document.body.appendChild(a);
+    a.click();
 
-  document.body.removeChild(a);
-  URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  } else {
+    try {
+      const bytes = new Uint8Array(arrayBuffer);
+      const len = bytes.byteLength;
+      let binary = '';
+
+      for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+      }
+
+      const uri = `${cacheDirectory}${fileName}`;
+      const base64 = btoa(binary);
+
+      await writeAsStringAsync(uri, base64, {
+        encoding: 'base64',
+      });
+
+      const asset = Asset.fromModule(uri);
+      await asset.downloadAsync();
+
+      if (asset.localUri) {
+        await shareAsync(asset.localUri);
+      } else {
+        ShowNativeFailedAlert({
+          // fixme localize
+          text: 'Download failed',
+        });
+      }
+    } catch(error) {
+      ShowNativeFailedAlert({
+        // fixme localize
+        text: JSON.stringify(error),
+      });
+
+      throw error;
+    }
+  }
 }
 
 export function readFileWeb(file: File): Promise<string> {
